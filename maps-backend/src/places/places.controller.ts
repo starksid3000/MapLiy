3dc23c584a1d07f28a6c2332e5928dc97d788cca
Your Name <your.email@example.com>
2026-02-26 15:43:23 +0530

    feat: add PATCH /places/:id endpoint to update a place.

--- maps-backend/src/places/places.controller.ts
+++ maps-backend/src/places/places.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body, Delete, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Param, ParseIntPipe, Patch } from '@nestjs/common';
 import { PlacesService } from './places.service';
 import { CreatePlaceDto } from './dto/create-place.dto';
+import { UpdatePlaceDto } from './dto/update-place.dto';
 
 @Controller('places')
 export class PlacesController {
@@ -16,11 +17,15 @@ export class PlacesController {
         return this.placesService.findAll();
     }
     @Delete(':id')
-    remove(@Param('id',ParseIntPipe) id: string) {
+    remove(@Param('id', ParseIntPipe) id: string) {
         return this.placesService.remove(+id);
     }
     @Get(':id')
-    findOne(@Param('id',ParseIntPipe) id: string) {
+    findOne(@Param('id', ParseIntPipe) id: string) {
         return this.placesService.findOne(+id);
+    }
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: string, @Body() updatePlaceDto: UpdatePlaceDto) {
+        return this.placesService.update(+id, updatePlaceDto);
     }
 }
